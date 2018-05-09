c867004e746306d98f443d76333e74693b26c895
mikima <mikimauri+github@gmail.com>
2018-05-09 17:36:25 +0200

    added italy bg

--- js/sketch.js
+++ js/sketch.js
@@ -4,8 +4,8 @@ var myCanvas;
 //By default, none is visible.
 const status = {
 	show_TX30: false,
-	show_PRCPTOT: false,
 	show_PR95PERC: false,
+	show_PRCPTOT: false,
 	years: '2011-2040'
 };
 
@@ -17,7 +17,8 @@ const graphVars = {
 	gray: 'gray',
 	red: '#ff0086',
 	blue: '#00fff8',
-	yellow: '#f9ff00'
+	yellow: '#f9ff00',
+	lightGray: '#d8d8d8'
 }
 
 // API Key for MapboxGL.
@@ -40,7 +41,7 @@ const myMap = mappa.staticMap(options);
 let mapImg;
 
 // datasets
-let fullData, province, prov_data;
+let fullData, province, confini, prov_data;
 //images
 let italy_bg;
 
@@ -52,10 +53,10 @@ let w, h;
 let canvas;
 let l1, l2, l3,
 	l1_45, l2_45, l3_45,
-	prov_layer
+	prov_layer, italy_layer
 
 //lens variables
-let lens_size = 300;
+let lens_size = 200;
 
 //timeline buttons
 let timeline_btns = [];
@@ -65,6 +66,7 @@ function preload() {
 	fullData = loadJSON('assets/climatechange_data.json');
 	province = loadJSON('assets/contorniProvince.json');
 	italy_bg = loadImage('assets/italy_bg.png');
+	confini = loadJSON('assets/contorni_italia.json');
 
 	//load and parse data for single provinces
 	d3.tsv("assets/climate-change-provincie.tsv").then(function(data) {
@@ -79,18 +81,6 @@ function preload() {
 				prov_data[d.provincia][d.variabile] = [];
 			}
 			prov_data[d.provincia][d.variabile].push(d);
-			// if (d.scenario in prov_data[d.provincia][d.variabile] === false) {
-			// 	prov_data[d.provincia][d.variabile][d.scenario] = [];
-			// }
-
-			// prov_data[d.provincia][d.variabile][d.scenario].push(d);
-
-			// if (d.variabile in prov_data[d.provincia][d.anno] === false) {
-			// 	prov_data[d.provincia][d.anno][d.variabile] = {};
-			// }
-			// if (d.proiezione in prov_data[d.provincia][d.anno][d.variabile] === false) {
-			// 	prov_data[d.provincia][d.anno][d.variabile][d.proiezione] = +d.valore;
-			// }
 		})
 	})
 }
@@ -116,11 +106,12 @@ function setup() {
 	//initialize map
 	l1 = new GeoLayer(fullData.values, 74, myMap, w, h);
 	l1_45 = new GeoLayer(fullData.values, 74, myMap, w, h);
-	l2 = new GeoLayer(fullData.values, -410, myMap, w, h);
-	l2_45 = new GeoLayer(fullData.values, -410, myMap, w, h);
-	l3 = new GeoLayer(fullData.values, 88, myMap, w, h);
-	l3_45 = new GeoLayer(fullData.values, 88, myMap, w, h);
+	l2 = new GeoLayer(fullData.values, 88, myMap, w, h);
+	l2_45 = new GeoLayer(fullData.values, 88, myMap, w, h);
+	l3 = new GeoLayer(fullData.values, -410, myMap, w, h);
+	l3_45 = new GeoLayer(fullData.values, -410, myMap, w, h);
 	prov_layer = new ShapeFileLayer(province, myMap, w, h);
+	italy_layer = new ShapeFileLayer(confini, myMap, w, h);
 
 	initMap(status.years);
 
@@ -186,26 +177,27 @@ function initMap(_years) {
 	l1_45.init(_years, 'TX30', 'RCP45');
 	l1_45.draw();
 
-	l2.color = graphVars.yellow;
-	l2.init(_years, 'PRCPTOT', 'RCP85');
+	l2.color = graphVars.blue;
+	l2.init(_years, 'PR95PERC', 'RCP85');
 	l2.draw();
 
-	l2_45.color = graphVars.yellow;
-	l2_45.init(_years, 'PRCPTOT', 'RCP45');
+	l2_45.color = graphVars.blue;
+	l2_45.init(_years, 'PR95PERC', 'RCP45');
 	l2_45.draw();
 
-	l3.color = graphVars.blue;
-	l3.init(_years, 'PR95PERC', 'RCP85');
+	l3.color = graphVars.yellow;
+	l3.init(_years, 'PRCPTOT', 'RCP85');
 	l3.draw();
 
 
-	l3_45.color = graphVars.blue;
-	l3_45.init(_years, 'PR95PERC', 'RCP45');
+	l3_45.color = graphVars.yellow;
+	l3_45.init(_years, 'PRCPTOT', 'RCP45');
 	l3_45.draw();
 
 	prov_layer.draw();
+	italy_layer.draw();
 
-	updateLayers(0, 0);
+	updateLayers(0, 0, false);
 }
 
 var selectedProvince = '';
@@ -284,96 +276,102 @@ g3.append("g")
 function updateLayers(_x, _y, _showPanel) {
 
 	clear();
-	//image(italy_bg,0,0,w,h);
+	image(italy_layer.image,0,0,w,h);
+
 	blendMode(MULTIPLY);
 	if (status.show_TX30) {
 		l1_45.invertMask(_x, _y, lens_size);
 		l1.mask(_x, _y, lens_size);
 	}
-	if (status.show_PRCPTOT) {
+	if (status.show_PR95PERC) {
 		l2_45.invertMask(_x, _y, lens_size);
 		l2.mask(_x, _y, lens_size);
 
 	}
-	if (status.show_PR95PERC) {
+	if (status.show_PRCPTOT) {
 		l3.mask(_x, _y, lens_size)
 		l3_45.invertMask(_x, _y, lens_size);
 	}
 	var newprov = prov_layer.mask(_x, _y, lens_size);
 
+	if(_showPanel == true) {
+		stroke('black')
+		noFill();
+		ellipse(_x, _y, lens_size);
+	}
+
 	if (newprov != null && newprov.properties.DEN_CMPRO != selectedProvince) {
 
 		selectedProvince = newprov.properties.DEN_CMPRO;
-		//title
 
 		d3.select('#nomeprovincia').html('Provincia di ' + selectedProvince);
 
 		//first graph
 		var TX30_data = d3.nest()
-				.key(function(e){ return e.proiezione})
-				.sortKeys(d3.descending)
-				.entries(prov_data[selectedProvince]['TX30'])
+			.key(function(e) { return e.proiezione })
+			.sortKeys(d3.descending)
+			.entries(prov_data[selectedProvince]['TX30'])
 
 		var paths1 = paths_g1.selectAll("path")
 			.data(TX30_data);
-		
+
 		paths1.enter()
 			.append("path")
-			.attr("fill", function(d){return d.key == 'RCP85' ? graphVars.red : graphVars.gray})
+			.attr("fill", function(d) { return d.key == 'RCP85' ? graphVars.red : graphVars.lightGray })
 			//.style("opacity",0.5)
 			.merge(paths1)
 			.transition()
-			.attr("d", function(d){console.log(d); return area(d.values)});
+			.attr("d", function(d) { return area(d.values) });
 
 		//first text
-		var value1_RCP85 = Math.round(+TX30_data[0].values.filter(function(d){ return d.anno == status.years})[0].valore);
-		var value1_RCP45 = Math.round(+TX30_data[1].values.filter(function(d){ return d.anno == status.years})[0].valore);
-		d3.select(desc_01).html('Nel trentennio ' + status.years + ', ci saranno circa <span class="scrittaMagenta">' + Math.abs(value1_RCP85) + ' giorni di caldo intenso in ' + (value1_RCP85 > 0 ? 'più' : 'meno') + '</span> rispetto ad oggi. Con l’adozione di politiche climatiche, i giorni in ' + (value1_RCP45 > 0 ? 'più' : 'meno') + ' rispetto ad oggi saranno circa ' + Math.abs(value1_RCP45) +'.')
+		var value1_RCP85 = Math.round(+TX30_data[0].values.filter(function(d) { return d.anno == status.years })[0].valore);
+		var value1_RCP45 = Math.round(+TX30_data[1].values.filter(function(d) { return d.anno == status.years })[0].valore);
+		d3.select(desc_01).html('Nel trentennio ' + status.years + ', ci saranno circa <span class="scrittaMagenta">' + Math.abs(value1_RCP85) + ' giorni di caldo intenso in ' + (value1_RCP85 > 0 ? 'più' : 'meno') + '</span> rispetto ad oggi. Con l’adozione di politiche climatiche, i giorni in ' + (value1_RCP45 > 0 ? 'più' : 'meno') + ' rispetto ad oggi saranno circa ' + Math.abs(value1_RCP45) + '.')
 
 		//second graph
 		var PR95PERC_data = d3.nest()
-				.key(function(e){ return e.proiezione})
-				.sortKeys(d3.descending)
-				.entries(prov_data[selectedProvince]['PR95PERC'])
+			.key(function(e) { return e.proiezione })
+			.sortKeys(d3.descending)
+			.entries(prov_data[selectedProvince]['PR95PERC'])
 
 		var paths2 = paths_g2.selectAll("path")
 			.data(PR95PERC_data);
-		
+
 		paths2.enter()
 			.append("path")
-			.attr("fill", function(d){return d.key == 'RCP85' ? graphVars.blue : graphVars.gray})
+			.attr("fill", function(d) { return d.key == 'RCP85' ? graphVars.blue : graphVars.lightGray })
 			//.style("opacity",0.5)
 			.merge(paths2)
 			.transition()
-			.attr("d", function(d){console.log(d); return area(d.values)});
+			.attr("d", function(d) { return area(d.values) });
 
 		//second text
-		var value2_RCP85 = Math.round(+PR95PERC_data[0].values.filter(function(d){ return d.anno == status.years})[0].valore);
-		var value2_RCP45 = Math.round(+PR95PERC_data[1].values.filter(function(d){ return d.anno == status.years})[0].valore);
+		var value2_RCP85 = Math.round(+PR95PERC_data[0].values.filter(function(d) { return d.anno == status.years })[0].valore);
+		var value2_RCP45 = Math.round(+PR95PERC_data[1].values.filter(function(d) { return d.anno == status.years })[0].valore);
 		d3.select(desc_02).html('Nel trentennio ' + status.years + ', nei giorni di pioggia intensa cadranno circa <span class="scrittaCyan">' + Math.abs(value2_RCP85) + ' mm di pioggia in ' + (value2_RCP85 > 0 ? 'più' : 'meno') + '</span> rispetto ad oggi. Con l’adozione di politiche climatiche, i mm in ' + (value2_RCP45 > 0 ? 'più' : 'meno') + ' rispetto ad oggi saranno circa ' + Math.abs(value2_RCP45) + '.');
-		
+
 		//third graph
 		var PRCPTOT_data = d3.nest()
-				.key(function(e){ return e.proiezione})
-				.sortKeys(d3.descending)
-				.entries(prov_data[selectedProvince]['PRCPTOT'])
+			.key(function(e) { return e.proiezione })
+			.sortKeys(d3.descending)
+			.entries(prov_data[selectedProvince]['PRCPTOT'])
 
 		var paths3 = paths_g3.selectAll("path")
 			.data(PRCPTOT_data);
-		
+
 		paths3.enter()
 			.append("path")
-			.attr("fill", function(d){return d.key == 'RCP85' ? graphVars.yellow : graphVars.gray})
+			.attr("fill", function(d) { return d.key == 'RCP85' ? graphVars.yellow : graphVars.lightGray })
 			//.style("opacity",0.5)
 			.merge(paths3)
 			.transition()
-			.attr("d", function(d){console.log(d); return area3(d.values)});
+			.attr("d", function(d) { return area3(d.values) });
 
 		//third text
-		var value3_RCP85 = Math.round(+PRCPTOT_data[0].values.filter(function(d){ return d.anno == status.years})[0].valore);
-		var value3_RCP45 = Math.round(+PRCPTOT_data[1].values.filter(function(d){ return d.anno == status.years})[0].valore);
+		var value3_RCP85 = Math.round(+PRCPTOT_data[0].values.filter(function(d) { return d.anno == status.years })[0].valore);
+		var value3_RCP45 = Math.round(+PRCPTOT_data[1].values.filter(function(d) { return d.anno == status.years })[0].valore);
 		d3.select(desc_03).html('Nel trentennio ' + status.years + ', nei mesi estivi scenderanno circa <span class="scrittaYellow">' + Math.abs(value3_RCP85) + ' mm di acqua in ' + (value3_RCP85 > 0 ? 'più' : 'meno') + '</span> rispetto ad oggi. Con l’adozione di misure di mitigazione dei cambiamenti climatici, la variazione sarà di circa ' + Math.abs(value3_RCP45) + ' mm in ' + (value3_RCP45 > 0 ? 'più' : 'meno') + '.');
-		
+
 	}
 
 }
@@ -419,6 +417,11 @@ function GeoLayer(_data, _maxval, _map, _width, _height) {
 				'y': pos.y,
 				'size': size
 			};
+			//add a second size if scenario is RCP45
+			if (_scenario == 'RCP45') {
+				p.outsize = map(Math.sqrt(Math.abs(item[_year][_variable]['RCP85'])), 0, maxVal, 0, distance);
+				p.innersize = map(Math.sqrt(Math.abs(item[_year][_variable]['RCP85'] - item[_year][_variable]['RCP45'])), 0, maxVal, 0, distance);
+			}
 			points.push(p);
 		})
 	}
@@ -426,9 +429,18 @@ function GeoLayer(_data, _maxval, _map, _width, _height) {
 		layer.clear();
 		//layer.background('white');
 		layer.noStroke();
+		layer.ellipseMode(CENTER);
 		layer.fill(this.color);
+		var c = this.color;
 		points.forEach(function(p) {
+			if(p.outsize != null){
+				layer.fill(c);
+				layer.ellipse(p.x, p.y, p.innersize);
+				// layer.fill(graphVars.lightGray);
+				// layer.ellipse(p.x, p.y, p.size);
+			} else {
 			layer.ellipse(p.x, p.y, p.size, p.size);
+			}
 		});
 
 		this.rendered = createImage(this.width, this.height);
@@ -522,8 +534,9 @@ function ShapeFileLayer(_geoJson, _map, _width, _height) {
 	}
 
 	this.draw = function() {
+		this.image = createImage(this.width, this.height);
 		layer.clear();
-		layer.background('white');
+		//layer.background('white');
 		layer.noFill();
 		layer.strokeWeight(0.2);
 		layer.stroke('black');
@@ -598,10 +611,6 @@ function ShapeFileLayer(_geoJson, _map, _width, _height) {
 	this.createPolygons();
 	// 
 }
-
-// function mouseMoved() {
-// 	updateLayers(mouseX, mouseY);
-// }
 
 function touchMoved() {
 	updateLayers(touches[0].x, touches[0].y, true);
